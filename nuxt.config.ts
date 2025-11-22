adb45b57f5cb555a5d223e98cd904f577e4fdeb9
vrijraj <vrijraj2396@gmail.com>
2025-11-22 08:48:00 +0400

    Add nuxt-gtag module for Google Analytics integration and update register.vue layout
    - Integrated nuxt-gtag module for enhanced Google Analytics tracking.
    - Updated nuxt.config.ts to include gtag configuration.
    - Improved layout in register.vue for better readability by formatting conditional rendering of ticket rows.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,8 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/scripts', 'vuetify-nuxt-module'],
+  modules: ['@nuxt/scripts', 'vuetify-nuxt-module', 'nuxt-gtag'],
+  gtag: {
+    id: "G-BV563RW7PJ",
+  },
 })
