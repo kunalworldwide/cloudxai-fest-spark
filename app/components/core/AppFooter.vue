6720d581784474dd89088235187a2e3daad2bd3c
vrijraj <vrijraj2396@gmail.com>
2025-12-03 17:25:29 +0530

    Correct spelling of "CLOUDxAI" in AppFooter.vue for consistency across the application.

--- app/components/core/AppFooter.vue
+++ app/components/core/AppFooter.vue
@@ -5,7 +5,7 @@
       <v-row class="mb-8 px-3" v-if="route.path !== '/register'">
         <v-col cols="12" md="12" class=" cta-btn ga-4 " >
           <span class="" style="color: #fff; font-size: 20px;">
-            <span style="font-weight: 600; font-size: 28px;">Secure your spot now at the CLOUDXAI conference.</span> <br> Be a part of the
+            <span style="font-weight: 600; font-size: 28px;">Secure your spot now at the CLOUDxAI conference.</span> <br> Be a part of the
             cloud shift from automation to intelligence movement.
           </span>
           <br>
